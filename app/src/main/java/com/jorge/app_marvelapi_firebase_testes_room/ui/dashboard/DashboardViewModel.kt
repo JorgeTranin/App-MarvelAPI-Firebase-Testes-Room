c5ac479c996ee888fe3f5c0f5f5634de7987143d
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-02-28 11:37:42 -0400

    Initial commit

---
+++ app/src/main/java/com/jorge/app_marvelapi_firebase_testes_room/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.jorge.app_marvelapi_firebase_testes_room.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/jorge/app_marvelapi_firebase_testes_room/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.jorge.app_marvelapi_firebase_testes_room.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
